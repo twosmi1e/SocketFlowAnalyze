7cfc6a869a63e0869d052375954c82e316a370ff
twosmi1e <lnyxyx@live.com>
2020-11-16 19:07:24 +0800

    mod

--- sniffer.py
+++ sniffer.py
@@ -18,18 +18,21 @@ self.src_address = socket.inet_ntoa(struct.pack("@I",self.src))
 self.dst_address = socket.inet_ntoa(struct.pack("@I",self.dst))
 ```
 """
+
 import socket
 import os
 import struct
+# import threading
+
 from ctypes import *
 
 # 监听的主机
-host = "172.29.75.189"
+host = "192.168.1.145"
 
-# ip头定义
+
 class IP(Structure):
-    _fields_ =[
-        ("ih1", c_ubyte, 4),
+    _fields_ = [
+        ("ihl", c_ubyte, 4),
         ("version", c_ubyte, 4),
         ("tos", c_ubyte),
         ("len", c_ushort),
@@ -46,29 +49,48 @@ class IP(Structure):
         return self.from_buffer_copy(socket_buffer)
 
     def __init__(self, socket_buffer=None):
-        # 协议字段与协议名称对应
-        self.protocol_map = {1:"ICMP", 6:"TCP", 17:"UDP"}
-        # 可读性更强的IP地址
+
+        # map protocol constants to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
+        # 转换为可读性更强的ip地址
         self.src_address = socket.inet_ntoa(struct.pack("@I", self.src))
         self.dst_address = socket.inet_ntoa(struct.pack("@I", self.dst))
 
+        # 可读性更强的协议
         try:
             self.protocol = self.protocol_map[self.protocol_num]
         except:
             self.protocol = str(self.protocol_num)
 
-# 下面的代码类似于之前的例子
 
-# 创建原始套接字， 然后绑定在公开接口上
+class ICMP(Structure):
+    _fields_ = [
+        ("type", c_ubyte),
+        ("code", c_ubyte),
+        ("checksum", c_ushort),
+        ("unused", c_ushort),
+        ("next_hop_mtu", c_ushort)
+    ]
+
+    def __new__(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        pass
+
+
+# 创建一个新的套接字，并绑定到公开接口上
 if os.name == "nt":
     socket_protocol = socket.IPPROTO_IP
 else:
     socket_protocol = socket.IPPROTO_ICMP
 
 sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
 sniffer.bind((host, 0))
 
-# 设置在捕获的数据包中包含ip头
+# 让捕获的数据中包含IP头
 sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
 # 在windows平台上，我们需要设置IOCTL以启用混杂模式
@@ -77,14 +99,27 @@ if os.name == "nt":
 
 try:
     while True:
+
         # 读取数据包
         raw_buffer = sniffer.recvfrom(65565)[0]
 
         # 将缓冲区的前20个字节按IP头进行解析
-        ip_header =IP(raw_buffer[0: 20])
+        ip_header = IP(raw_buffer[0:20])
 
         # 输出协议和通信双方IP地址
-        print("protocol: %s %s -> %s"%(ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+        print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+
+
+        # 如果是ICMP协议，进行处理
+        if ip_header.protocol == "ICMP":
+            # 计算ICMP包的起始位置
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
+
+            # 解析ICMP数据
+            icmp_header = ICMP(buf)
+
+            print("ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code))
 
 # 处理Ctrl+C
 except KeyboardInterrupt:
